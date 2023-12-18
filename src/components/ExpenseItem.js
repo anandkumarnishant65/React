a9013f37dc940eff922b2341dd6c23ce7c689a92
anandkumarnishant65 <anandkumar65@.com>
2023-12-18 21:35:35 +0530

    2nd commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,19 @@
+import './ExpenseItem.css';
+
 function ExpenseItem(){
-    return <div>
-        <h2>Expense Item</h2>
-        <div>Food Rs 10</div>
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance'
+    const expenseAmount =294.67
 
-        <div>Petrol Rs 100</div>
+    return <div className = "expense-item">
+        <div>{expenseDate.toISOString()}</div>
 
-        <div>Movies Rs 200</div>
+        <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+        </div>
+
+        
     </div>
 }
 
