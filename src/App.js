9b1d26eff3d0963adf5c632291bc4dcd0dc6d3c6
anandkumarnishant65 <anandkumar65@.com>
2023-12-19 17:54:10 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,56 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses=[
+    {
+      id:'e1',
+      title:'Tooth Paste',
+      amount:55,
+      date:new Date(2022,7,23),
+    },
+    {
+      id:'e2',
+      title:'Deodorant',
+      amount:220,
+      date:new Date(2022,7,24),
+    },
+    {
+      id:'e3',
+      title:'TV',
+      amount:22000,
+      date:new Date(2022,3,2),
+    },
+    {
+      id:'e4',
+      title:'T-shirt',
+      amount:550,
+      date:new Date(2022,7,28),
+    },
+  ]
+  
   return (
     <div>
       <h2>Lets get started</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}>
+      </ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}>
+      </ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}>
+      </ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}>
+      </ExpenseItem>
     </div>
   );
 }
