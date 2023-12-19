79bdb1240aede037982aa5171da358075f152516
anandkumarnishant65 <anandkumar65@.com>
2023-12-19 23:56:28 +0530

    smaller and readable

--- src/App.js
+++ src/App.js
@@ -9,25 +9,29 @@ function App() {
       id:'e1',
       title:'Tooth Paste',
       amount:55,
-      date:new Date(2022,7,23),
+      date:new Date(2022,0,23),
+      location: 'bhopal',
     },
     {
       id:'e2',
       title:'Deodorant',
       amount:220,
       date:new Date(2022,7,24),
+      location: 'jaipur',
     },
     {
       id:'e3',
       title:'TV',
       amount:22000,
       date:new Date(2022,3,2),
+      location: 'delhi',
     },
     {
       id:'e4',
       title:'T-shirt',
       amount:550,
       date:new Date(2022,7,28),
+      location: 'lucknow',
     },
   ]
   
@@ -37,22 +41,26 @@ function App() {
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
-        date={expenses[0].date}>
+        date={expenses[0].date}
+        location={expenses[0].location}>
       </ExpenseItem>
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
-        date={expenses[1].date}>
+        date={expenses[1].date}
+        location={expenses[1].location}>
       </ExpenseItem>
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
-        date={expenses[2].date}>
+        date={expenses[2].date}
+        location={expenses[2].location}>
       </ExpenseItem>
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
-        date={expenses[3].date}>
+        date={expenses[3].date}
+        location={expenses[3].location}>
       </ExpenseItem>
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,14 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 
 function ExpenseItem(props){
     
-    return <div className = "expense-item">
-        <div>{props.date.toISOString()}</div>
-
-        <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">Rs{props.amount}</div>
-        </div>
-
-        
+    return (
+     <div className = "expense-item">
+        <ExpenseDate date={props.date}/>
+        <ExpenseDetails amount={props.amount} title={props.title} location={props.location}/>    
     </div>
-}
+)}
 
 export default ExpenseItem;
