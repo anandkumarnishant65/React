df1ae8a9eec06eebbf72dc7e18a88eea6c3a8599
anandkumarnishant65 <anandkumar65@.com>
2023-12-21 02:28:00 +0530

    organize

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Expenses from './components/Expense/Expenses';
 
-import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   const expenses=[
@@ -37,31 +37,8 @@ function App() {
   
   return (
     <div>
-      <h2>Lets get started</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-        location={expenses[0].location}>
-      </ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-        location={expenses[1].location}>
-      </ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-        location={expenses[2].location}>
-      </ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-        location={expenses[3].location}>
-      </ExpenseItem>
+      <h2>Lets get started!</h2>
+      <Expenses items={expenses}/>
     </div>
   );
 }
--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,14 +1,16 @@
+import Card from '../UI/card';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 
+
 function ExpenseItem(props){
     
     return (
-     <div className = "expense-item">
+     <Card className = "expense-item">
         <ExpenseDate date={props.date}/>
         <ExpenseDetails amount={props.amount} title={props.title} location={props.location}/>    
-    </div>
+    </Card>
 )}
 
 export default ExpenseItem;
