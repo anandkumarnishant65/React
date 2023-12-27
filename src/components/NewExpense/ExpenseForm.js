2b074e7e9a0ec7163422b0913b12457a752d9acf
anandkumarnishant65 <anandkumar65@.com>
2023-12-27 13:11:47 +0530

    stateDesignPattern2

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,19 +4,27 @@ import './ExpenseForm.css'
 
 const ExpenseForm=()=>{
 
-    const [EnteredTitle,setEnteredTitle]=useState("");
-    const [EnteredAmount,setEnteredAmount]=useState("");
-    const [EnteredDate,setEnteredDate]=useState("");
+    // const [EnteredTitle,setEnteredTitle]=useState("");
+    // const [EnteredAmount,setEnteredAmount]=useState("");
+    // const [EnteredDate,setEnteredDate]=useState("");
+    const [userInput, setUserInput]=useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: '',
+    })
 
 
     const titleChangeHandler=(event)=>{
-        setEnteredTitle(event.target.value)
+        // setEnteredTitle(event.target.value)
+        setUserInput({...userInput,enteredTitle: event.target.value,})
     }
     const amountChangeHandler=(event)=>{
-        setEnteredAmount(event.target.value)
+        //setEnteredAmount(event.target.value)
+        setUserInput({...userInput,enteredAmount: event.target.value,})
     }
     const dateChangeHandler=(event)=>{
-        setEnteredDate(event.target.value)
+        //setEnteredDate(event.target.value)
+        setUserInput({...userInput,enteredDate: event.target.value,})
     }
 
     return (
