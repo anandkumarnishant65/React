3b27c2cdc423b2d6a79972c83e7da0078de575b9
anandkumarnishant65 <anandkumar65@.com>
2023-12-28 17:58:43 +0530

    filter

--- src/components/Expense/ExpenseDetails.js
+++ src/components/Expense/ExpenseDetails.js
@@ -1,29 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import './ExpenseItem.css';
 
 function ExpenseDetails(props){
-    
-    const [title,setTitle]=useState(props.title);
-    const clickHandler = ()=>{
-        setTitle('Updated!!!');
-        console.log(title)
-    }
 
-    const [amount,setAmount]=useState(props.amount);
-    const clickHandle = ()=>{
-        setAmount('100');
-        console.log(amount)
-    }
 
 
     return(
         <div className="expense-item__description">
-            <h2>{title}</h2>
-            <div className="expense-item__price">Rs{amount}</div>
-            <div className="expense-item__location">{props.location}</div>
-            <button onClick={clickHandler}>change Title</button>
-            <button onClick={clickHandle}>change Amount</button>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">Rs{props.amount}</div>
+            
         </div>
     )
 
--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,34 +1,36 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 import Kard from "../UI/Kard";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 function Expenses(props){
+  const [filteredYear,setFilteredYear]=useState('2020')
+  const filterChangeHandler=selectedYear=>{
+    setFilteredYear(selectedYear);
+  }
     return (
     <Kard className="expenses">
-      
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
-        date={props.items[0].date}
-        location={props.items[0].location}>
+        date={props.items[0].date}>
       </ExpenseItem>
       <ExpenseItem
         title={props.items[1].title}
         amount={props.items[1].amount}
-        date={props.items[1].date}
-        location={props.items[1].location}>
+        date={props.items[1].date}>
       </ExpenseItem>
       <ExpenseItem
         title={props.items[2].title}
         amount={props.items[2].amount}
-        date={props.items[2].date}
-        location={props.items[2].location}>
+        date={props.items[2].date}>
       </ExpenseItem>
       <ExpenseItem
         title={props.items[3].title}
         amount={props.items[3].amount}
-        date={props.items[3].date}
-        location={props.items[3].location}>
+        date={props.items[3].date}>
       </ExpenseItem>
     </Kard>
     )
