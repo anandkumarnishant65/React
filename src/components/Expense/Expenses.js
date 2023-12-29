638be470dc7847be7fae787c6bb7db52f714336f
anandkumarnishant65 <anandkumar65@.com>
2023-12-29 20:33:17 +0530

    conditionalRendering3

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -13,23 +13,25 @@ function Expenses(props){
   const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString()===filteredYear
   })
-    return (
-    <Kard className="expenses">
-      <ExpensesFilter selected={filteredYear} 
-      onChangeFilter={filterChangeHandler}/>
 
-      {filteredExpenses.length === 0 && <p>No expenses found.</p>}
-      {filteredExpenses.length > 0 && 
-        filteredExpenses.map((expense) => (
-        <ExpenseItem
+  let expensesContent = <p>No expenses found.</p>;
+  if (filteredExpenses.length>0){
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
           key={expense.id}
           title={expense.title} 
           amount={expense.amount} 
           date={expense.date}
-        />)
-      )} 
-      
-      
+        />
+    ))
+  }
+
+    return (
+    <Kard className="expenses">
+      <ExpensesFilter selected={filteredYear} 
+      onChangeFilter={filterChangeHandler}
+      />
+      {expensesContent}
     </Kard>
     )
 }
