c75b0a4d7cedd350ca37e17b1d8576fa39f6db4b
anandkumarnishant65 <anandkumar65@.com>
2023-12-29 10:42:17 +0530

    renderingOptimization

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
 // import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import Expenses from './components/Expense/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES=[
+  {
+    id:'e1',
+    title:'Tooth Paste',
+    amount:55,
+    date:new Date(2022,0,23),
+    location: 'bhopal',
+  },
+  {
+    id:'e2',
+    title:'Deodorant',
+    amount:220,
+    date:new Date(2022,7,24),
+    location: 'jaipur',
+  },
+  {
+    id:'e3',
+    title:'TV',
+    amount:22000,
+    date:new Date(2022,3,2),
+    location: 'delhi',
+  },
+  {
+    id:'e4',
+    title:'T-shirt',
+    amount:550,
+    date:new Date(2022,7,28),
+    location: 'lucknow',
+  },
+]
+
 function App() {
-  const expenses=[
-    {
-      id:'e1',
-      title:'Tooth Paste',
-      amount:55,
-      date:new Date(2022,0,23),
-      location: 'bhopal',
-    },
-    {
-      id:'e2',
-      title:'Deodorant',
-      amount:220,
-      date:new Date(2022,7,24),
-      location: 'jaipur',
-    },
-    {
-      id:'e3',
-      title:'TV',
-      amount:22000,
-      date:new Date(2022,3,2),
-      location: 'delhi',
-    },
-    {
-      id:'e4',
-      title:'T-shirt',
-      amount:550,
-      date:new Date(2022,7,28),
-      location: 'lucknow',
-    },
-  ]
+  const [expenses,setExpenses] = useState(DUMMY_EXPENSES)
+
   const addExpenseHandler=(expense)=>{
-    console.log('In App.js')
-    console.log(expense)
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    })
   }
   return (
     <div>
